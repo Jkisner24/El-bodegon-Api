bdeeb6ccfcc65bb045762a9780f47bba18e417e2
LautyFarías247 <lauty0618@gmail.com>
2023-03-16 16:08:23 -0300

    gitignore arreglado????????

--- README.md
+++
@@ -1,1 +0,0 @@
-# El-bodegon-Api
--- index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./src/app')
-const connectDB = require('./src/database');
-
-
-connectDB()
-app.listen(3001, () => {
-    console.log('Listening on port 3001')
-})
