ea09a9993af5d560c4792039c766adfe178e1f57
LautyFarías247 <lauty0618@gmail.com>
2023-03-23 00:32:49 -0300

    Access-Control-Allow

--- src/app.js
+++ src/app.js
@@ -9,11 +9,14 @@ const cors = require('cors')
 
 const app = express();
 
-app.use(cors({   
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-}))
+app.use(cors())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
 app.use(express.json())
 app.use(morgan('dev'));
 
