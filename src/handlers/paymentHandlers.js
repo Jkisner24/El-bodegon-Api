20291c6beb05ed9ccbe4c9ed49920af3ac9e92b0
LautyFarías247 <lauty0618@gmail.com>
2023-03-23 13:05:06 -0300

    MERCADO PAGO FUNCIONA

--- src/handlers/paymentHandlers.js
+++ src/handlers/paymentHandlers.js
@@ -2,32 +2,41 @@ const mercadopago = require("mercadopago");
 
 
 const payment = async (req, res) =>{
-    const {name, price=100, description, quantity=2} = req.body;
-// Crea un objeto de preferencia
-let preference = {
-    items: [
-    {
-        title: name,
-        currency_id: "ARS",
-        picture_url: "url",
-        description: description,
-        category_id: "art",
-        quantity: quantity,
-        unit_price: price
-    }],
-    back_urls: {
-        success: "https://localhost:3001",
-        failure: "",
-        pending: ""
-    },
-    auto_return: "approved",
-    binary_mode: true //no deja pagos pendientes
-  };
-  
-  mercadopago.preferences
-    .create(preference)
-    .then((response)=>res.status(200).send({response}))
-    .catch((error)=>res.status(404).send(error.message));
+    try {
+        
+    // Crea un objeto de preferencia
+    let preference = {
+        items: [],
+        back_urls: {
+            success: "https://localhost:3001",
+            failure: "",
+            pending: ""
+        },
+        auto_return: "approved",
+        binary_mode: true //no deja pagos pendientes
+      };
+    
+    req.body.forEach(item => {
+        preference.items.push(
+            {
+                title: item.name,
+                currency_id: "ARS",
+                picture_url: item.image,
+                description: item.description,
+                category_id: "art",
+                quantity: item.quantity,
+                unit_price: item.price
+            }
+        )
+      });
+      
+      mercadopago.preferences
+        .create(preference)
+        .then((response)=>res.status(200).send({response}))
+        .catch((error)=>res.status(404).send(error.message));
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
  
 }
 
