49cdfdb15a7cdeff645c11a9d841b76a8914f939
LautyFarías247 <lauty0618@gmail.com>
2023-03-26 20:57:12 -0300

    allow accses

--- src/app.js
+++ src/app.js
@@ -20,7 +20,13 @@ mercadopago.configure({
 
 
 const app = express();
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://el-bodegon-nuevo-repo.vercel.app/'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'));
