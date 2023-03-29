b7091ac630c6e38c7a898b2aa9d106ee9443ab89
LautyFarías247 <lauty0618@gmail.com>
2023-03-29 14:58:47 -0300

    NO CORS

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'));
 
