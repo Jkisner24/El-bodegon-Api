118bdc46a8e680b657c66e7f760bc6b85282747c
lvalcke <lvalcke88@gmail.com>
2023-04-02 04:07:13 -0500

    styled e-mail

--- src/libs/nodemailer.js
+++ src/libs/nodemailer.js
@@ -15,13 +15,17 @@ sendMail = async ( toMail, name ) => {
         }
         const mail = {
             from: 'elbodegondetony2023@gmail.com',
-            to: toMail,
+            to: 'lvalcke88@gmail.com',
             subject: 'Confirmacion de registro El bodegon de Tony',
             html: 
-            `<div style= "width: '500px'; borderRadius: '20px'; backgroundColor: 'black'; color: 'white'; display: 'flex'" >
-                <h1>Hola, ${name}!!!</h1>
-                <h2>Bienvenido a El Bodegon De Tony</h2>
-                <p>Es un placer para nosotros darte la bienvenida</p>
+            `<div style= "background-color:black; border-radius:20px; padding:50px; text-align:center; color: white;" >
+                <div style= "border: 10px solid rgb(235, 14, 14); border-radius: 20px; padding:50px;" >
+                    <h1>Hola, ${name}!!!</h1>
+                    <h2 style= "color: white;" >Bienvenido a "El Bodegon De Tony"</h2>
+                    <h2 style= "color: white;" >Es un placer para nosotros darte la bienvenida</h2>
+                    <h2 style= "color: white;" >y ofrecerte los mejores platos a los mejores precios</h2>
+                    <a style= "color: rgb(235, 158, 14); margin: 100px;" href="https://el-bodegon-nuevo-repo.vercel.app/">Volver a la pagina principal</a>
+                </div>
              </div>`
         }
         
